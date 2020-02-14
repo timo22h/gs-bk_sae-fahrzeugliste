366f607f9104081da196ee6f83b5207fd6d033f0
Timo Heyden <timo.heyden@de.kaercher.com>
2020-02-14 18:35:20 +0100

    added AusgabeMenu()

--- sae-fahrzeugliste/sae-fahrzeugliste.cpp
+++ sae-fahrzeugliste/sae-fahrzeugliste.cpp
@@ -1,13 +1,32 @@
 // sae-fahrzeugliste.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+// Einbinden von Systembibliotheken
 #include "stdafx.h"
 #include <iostream>
 #include <cmath>
 #include "string.h"
 using namespace std;
 
+// Deklaration der globalen Methoden
+void AusgabeMenu();
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	AusgabeMenu();
+}
+
+//Funktion um das Menu initial auszugeben
+void AusgabeMenu()
+{
+	cout << "\nORGANISATION DER FAHRZEUGDATEN\n" << endl;
+	cout << "1 = Anlegen eines neuen Fahrzeugs" << endl;
+	cout << "2 = Suchen eines bestimmten Fahrzeugs" << endl;
+	cout << "3 = Ausgabe aller Fahrzeuge" << endl;
+	cout << "4 = Loeschen eines Fahrzeugs" << endl;
+	
+	// Eventuell Berechnen von Steuern
+	cout << "5 = Berechnen von Steuern für ein bestimmtes Fahrzeug" << endl;
+
+	cout << "Ihre Wahl: ";
 }
