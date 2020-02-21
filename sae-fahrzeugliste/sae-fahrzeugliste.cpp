4ed6fb5892eb79a0f30f2800cf7115767cf968ba
timo22h <44976256+timo22h@users.noreply.github.com>
2020-02-21 20:22:35 +0100

    added search function updated user interface

--- sae-fahrzeugliste/sae-fahrzeugliste.cpp
+++ sae-fahrzeugliste/sae-fahrzeugliste.cpp
@@ -37,7 +37,8 @@ void AusgabeMenu();
 
 // Deklaration Klasse Fahrzeug
 #pragma region Klasse Fahrzeug
-class Fahrzeug {
+class Fahrzeug
+{
 protected:
 	char kennzeichen[10];
 	int erstzulassung;
@@ -63,8 +64,7 @@ private:
 	int schadstoffklasse;
 
 public:
-	Pkw(char *kennzeichen, int jahr, int hubraum, int leistung, short schadstoffklasse = 0) :
-		Fahrzeug(kennzeichen, erstzulassung), hubraum(hubraum), leistung(leistung), schadstoffklasse(schadstoffklasse) {};
+	Pkw(char *kennzeichen, int jahr, int hubraum, int leistung, short schadstoffklasse = 0) : Fahrzeug(kennzeichen, erstzulassung), hubraum(hubraum), leistung(leistung), schadstoffklasse(schadstoffklasse){};
 	Pkw();
 	virtual void Print();
 	virtual Fahrzeug *Copy();
@@ -73,13 +73,13 @@ public:
 
 // Deklaration der Klasse Motorrad - abgeleitet von der Klasse Fahrzeug
 #pragma region Klasse Motorrad
-class Motorrad : public Fahrzeug {
+class Motorrad : public Fahrzeug
+{
 private:
 	int hubraum;
 
 public:
-	Motorrad(char *kennzeichen, int jahr, int hubraum) :
-		Fahrzeug(kennzeichen, jahr), hubraum(hubraum) {};
+	Motorrad(char *kennzeichen, int jahr, int hubraum) : Fahrzeug(kennzeichen, jahr), hubraum(hubraum){};
 	Motorrad();
 	virtual void Print();
 	virtual Fahrzeug *Copy();
@@ -88,15 +88,18 @@ public:
 
 // Deklaration von Klasse Node
 #pragma region Klase Node
-class Node {
+class Node
+{
 private:
 	Fahrzeug *object;
 	Node *left, *right;
 	int CmpNode(Node *);
 
 public:
-	Node(Fahrzeug *obj) { object = obj->Copy();
-	left = right = NULL;
+	Node(Fahrzeug *obj)
+	{
+		object = obj->Copy();
+		left = right = NULL;
 	}
 	void Print();
 	Fahrzeug *Suchen(char *kennzeichen);
@@ -122,9 +125,9 @@ Fahrzeug::Fahrzeug()
 
 void Fahrzeug::Print()
 {
-	cout << endl << kennzeichen << endl;
-	cout << "Erstzulassung: \t\t" <<
-		erstzulassung << endl;
+	cout << endl
+		 << kennzeichen << endl;
+	cout << "Erstzulassung: \t\t" << erstzulassung << endl;
 }
 #pragma endregion
 
@@ -141,8 +144,8 @@ Pkw::Pkw()
 
 void Pkw::Print()
 {
-	const char *Klassen[3] = { "Sauber", "Dreckschleuder", "Diesel" };
-	
+	const char *Klassen[3] = {"Sauber", "Dreckschleuder", "Diesel"};
+
 	// Ausgabe der Infos der Basisklasse Fahrzeug - ist unbedingt notwendig!
 	Fahrzeug::Print();
 
@@ -152,7 +155,7 @@ void Pkw::Print()
 	cout << "Schadstoffklasse:[0..2]\t" << Klassen[schadstoffklasse] << endl;
 }
 
-Fahrzeug* Pkw::Copy()
+Fahrzeug *Pkw::Copy()
 {
 	// liefert identische Kopie des aktuellen Objekts
 	return new Pkw(*this);
@@ -175,7 +178,7 @@ void Motorrad::Print()
 	cout << "Hubraum:\t\t" << hubraum << endl;
 }
 
-Fahrzeug* Motorrad::Copy()
+Fahrzeug *Motorrad::Copy()
 {
 	return new Motorrad(*this);
 }
@@ -200,7 +203,8 @@ Node *Node::AddObject(Fahrzeug *obj)
 	{
 		return newnode;
 	}
-	do {
+	do
+	{
 		if (root->CmpNode(newnode) > 0)
 		{
 			// Gehört der neue Knoten in den linken oder rechten Teilbaum?
@@ -213,23 +217,53 @@ Node *Node::AddObject(Fahrzeug *obj)
 			else
 				root = root->left;
 		}
+		else if (root->right == NULL)
+		{
+			root->right = newnode;
+			inserted = 1;
+		}
 		else
-			if (root->right == NULL)
-			{
-				root->right = newnode;
-				inserted = 1;
-			}
-			else
-				root = root->right;
+			root = root->right;
 	} while (inserted == 0);
 	return this;
+}
+
+Fahrzeug *Node::Suchen(char *kennzeichen)
+{
+	int suchstatus = 1;
+	Node *Start = this;
+
+	while (Start!=NULL && suchstatus!=0)
+	{
+		suchstatus = strcmp(kennzeichen, Start->object->Kennzeichen());
+		if (suchstatus != 0) // Suche fotsetzen?
+		{
+			if (suchstatus < 0) // Kennzeichen kleiner
+			{
+				Start = Start->left;
+			}
+			else // Kennzeichen größer
+			{
+				Start = Start->right;
+			}
+		}
+	}
+	if (suchstatus != 0)
+	{
+		return NULL;
+	}
+	else
+	{
+		return Start->object;
+	}
+	
 }
 
 void Node::Print()
 {
 	if (left != NULL)
 		left->Print(); // Daten des li. Teilbaums ausgeben . Teilbaums ausgeben
-	object->Print(); // Ausgeben der Objektdaten
+	object->Print();   // Ausgeben der Objektdaten
 	if (right != NULL)
 		right->Print(); // Daten des re. Teilbaums ausgeben
 }
@@ -238,13 +272,14 @@ void Node::Print()
 //Funktion um das Menu initial auszugeben
 void AusgabeMenu()
 {
-	cout << "\nORGANISATION DER FAHRZEUGDATEN\n" << endl;
+	cout << "\nORGANISATION DER FAHRZEUGDATEN\n"
+		 << endl;
 	cout << "1 = Anlegen eines neuen PKWs" << endl;
 	cout << "2 = Anlegen eines neuen Motorrads" << endl;
 	cout << "3 = Suchen eines bestimmten Fahrzeugs" << endl;
 	cout << "4 = Ausgabe aller Fahrzeuge" << endl;
 	cout << "5 = Loeschen eines Fahrzeugs" << endl;
-	
+
 	// Eventuell Berechnen von Steuern
 	cout << "6 = Berechnen von Steuern fuer ein bestimmtes Fahrzeug" << endl;
 
@@ -256,21 +291,43 @@ int main()
 	int eingabe;
 	Node *root = NULL;
 
-	do {
+	do
+	{
 		AusgabeMenu();
 		cin >> eingabe;
 
 		switch (eingabe)
 		{
 		case 1:
+			// Neuen PKW anlegen
 			root = root->AddObject(new Pkw);
 			break;
 		case 2:
+			// Neues Motorrad anlegen
 			root = root->AddObject(new Motorrad);
 			break;
+		case 3:
+			// Suche nach Kennzeichen
+			char kennzeichen[10];
+			Fahrzeug *Fz;
+			cin >> kennzeichen;
+			Fz = root->Suchen(kennzeichen);
+			if (Fz != NULL)
+			{
+				Fz->Print();
+			}
+			break;
 		case 4:
+			// Ausgabe aller eingetragenen Fahrzeuge
 			root->Print();
+			break;
+		case 5:
+			// Löschen eines bestimmten Fahrzeugs anhand des Kennzeichens
+			break;
+		case 6:
 			break;
 		}
 	} while (eingabe != 'x');
+	system("cls");
+	system("pause");
 }
